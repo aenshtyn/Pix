5dba7110fff5d51ecf6f3ec2e825b2de4ae3f434
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-22 14:58:05 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mohamed/Documents/Moringa-School-Projects/Moringa Core/Week 8/IP/Pix/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
