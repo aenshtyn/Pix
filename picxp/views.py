63a1507f97453617d4539400c1f417353da589d3
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-25 22:06:43 +0300

    created function to display pictures by post date

--- picxp/views.py
+++ picxp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Picxp')
+    return render(request, 'welcome.html')
+
+
+def latest_pics(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
