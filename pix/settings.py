fbc31b54f151bd58b37d7b5b8b97d5647eb9feff
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-25 22:44:46 +0300

    installed and imported bootstrap to the base template and settings file

--- pix/settings.py
+++ pix/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'picxp'
-    
+    'picxp',
+    'bootstrap3'
+
 ]
 
 MIDDLEWARE = [
