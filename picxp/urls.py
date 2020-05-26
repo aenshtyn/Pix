555c00eabbd447ecf792d18389348a4b7840d26b
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-26 06:48:52 +0300

    updated the url patterns to display views on site bootup

--- picxp/urls.py
+++ picxp/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^latest/$',views.latest_pics,name='latestPics')
+    url('^$',views.latest_pics,name='latestPics')
 ]
