fabe146f3b0c4205884fb7b775236478016d4a67
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-01 19:12:28 +0300

    created two new models for the categpry and location as per the project requirements

--- picxp/models.py
+++ picxp/models.py
@@ -7,13 +7,18 @@ class location(models.Model):
     def __str__(self):
         return self.name
 
+class category(models.Model):
+    name = models.CharField(max_length = 30 )
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =60)
     description = models.TextField()
     location = models.ForeignKey(Author)
-    category = models.ManyToManyField(tags)
+    category = models.ManyToManyField(category)
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
